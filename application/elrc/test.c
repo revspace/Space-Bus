0346eddad66a4bf005128caf638ea44c4f6f1f0a
Sonologic <gmc@sonologic.nl>
2011-08-02 08:47:10 +0200

    Add code to take input from switches.

--- application/elrc/test.c
+++ application/elrc/test.c
@@ -23,10 +23,18 @@ void byte_sent(void *data) {
   test->sent=1;
 }
 
+void send(uint8_t b,struct hw_callbacks *cb,test_data_t *data) {
+        data->sent=0;
+	cb->d_begin_transmission();
+	cb->d_send_byte(b);
+        while(data->sent==0);
+        cb->d_end_transmission();
+}
+
 int main(void) {
   struct hw_callbacks cb;
-  uint8_t byte;
   test_data_t data;
+  uint8_t lamp_state=0;
 
   /* initialize spacebus link layer */
   cb.u_byte_received=&byte_received;
@@ -35,16 +43,24 @@ int main(void) {
 
   tiny485(&cb);
 
-  byte=1;
+	// set inputs
+  DDRB &= ~_BV(3);
+  DDRB &= ~_BV(4);
+
+	// enable pull-up
+  PORTB |= _BV(3);
+  PORTB |= _BV(4);
+ 
   while(1) {
-        data.sent=0;
-
-	cb.d_begin_transmission();
-	cb.d_send_byte(byte);
-        while(data.sent==0);
-        cb.d_end_transmission();
-
-	if( ++byte > 4) byte=1;
-	_delay_ms(1);
+	if(!(PINB&_BV(3))) {
+		send(1,&cb,&data);
+		_delay_ms(1000);
+	}
+	if(!(PINB&_BV(4))) {
+		lamp_state=1-lamp_state;
+		send(lamp_state?3:4,&cb,&data);
+		_delay_ms(1000);
+	}
+	_delay_ms(100);
   }
 }
