5fc699fe391458bb9ed84d1e6dc77a00f57a5837
Roy Smeding <roy.smeding@gmail.com>
2011-11-20 17:29:54 +0100

    Did some work on the tiny-dev board.

--- application/tests/sblp-recv-test.c
+++ application/tests/sblp-recv-test.c
@@ -7,18 +7,18 @@
 void frame_sent() { }
 
 void frame_received(struct sblp_header *header, uint8_t *payload) {
-	if(payload[0])	PORTB |=  _BV(PB3);
-	else		PORTB &= ~_BV(PB3);
+	if(payload[0])	PORTB |=  _BV(PA0);
+	else		PORTB &= ~_BV(PA0);
 
-	if(payload[1])	PORTB |=  _BV(PB4);
-	else		PORTB &= ~_BV(PB4);
+	if(payload[1])	PORTB |=  _BV(PA1);
+	else		PORTB &= ~_BV(PA1);
 }
 
 int main(void) {
 	hw_init();
 	sblp_init();
 	
-	DDRB |= _BV(PB3) | _BV(PB4);
+	DDRB |= _BV(PA0) | _BV(PA1);
 
 	while(1) _delay_ms(500);
 }
